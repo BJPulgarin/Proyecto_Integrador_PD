b60951362c8c08d65897818e628c948b2d0ac307
Brajan Pulgarin <bjpulgarin@gmail.com>
2023-10-10 15:39:15 -0500

    Parte 1 del proyecto

---
+++ Parte1.py
@@ -0,0 +1,12 @@
+from datasets import load_dataset
+import numpy as np
+
+dataset = load_dataset("mstz/heart_failure")
+data = dataset["train"]
+
+listaEdades = data["age"]
+edadesArray = np.array(listaEdades)
+
+edadPromedio = int(round(np.average(edadesArray)))
+
+print(edadPromedio)
