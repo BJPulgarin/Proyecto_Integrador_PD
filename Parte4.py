8a57926259ee4dacd22a8ca30faf62df98f42096
Brajan Pulgarin <bjpulgarin@gmail.com>
2023-10-26 13:59:05 -0500

    Solución parte 4

--- Parte4.py
+++ Parte4.py
@@ -1,5 +1,16 @@
 import requests
 
-response = requests.get("https://huggingface.co/datasets/mstz/heart_failure/raw/main/heart_failure_clinical_records_dataset.csv")
+def ObtenerCsv(url: str):
+    response = requests.get(url)
+    try:
+        api_data = response.text
+        with open('data.csv', 'w') as f:
+            f.write(api_data)
+            print('Se ha generado el archivo!')
+    except requests.exceptions.RequestException:
+        print('Ocurrió un problema con la solicitud HTTP')
+    except Exception as e:
+        print(f"Ocurrió un error inesperado: {e}")
 
-print(response)
+
+ObtenerCsv("https://huggingface.co/datasets/mstz/heart_failure/raw/main/heart_failure_clinical_records_dataset.csv")
