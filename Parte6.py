1bb355fc6a5f40b12adee2f38c2e85d83204fecc
Brajan Pulgarin <bjpulgarin@gmail.com>
2023-11-05 01:49:58 -0500

    Solución Parte 6

--- Parte6.py
+++ Parte6.py
@@ -1,14 +1,17 @@
 import pandas as pd
+import sys
 
-data_original = pd.read_csv("./data.csv")
+csv = sys.argv[1]
+data_original = pd.read_csv(csv)
 
 
 def LimpiarData(data: pd.DataFrame):
+    print('\n')
     # hay faltantes?
     faltantes = data.isna().sum()
     print('Datos faltantes por columna: ')
     print(faltantes)
-    print('\n')
+    print('')
 
     # hay registros duplicados?
     duplicados = data.duplicated().sum()
@@ -16,22 +19,21 @@ def LimpiarData(data: pd.DataFrame):
         print('No hay registros duplicados')
     else:
         print('Existen registros duplicados')
-    print('\n')
+    print('')
 
     # valores atípicos
 
     # tipos de datos
-    print('Tipos de datos: ')
-    print(data.dtypes)
+    # print('Tipos de datos: ')
+    # print(data.dtypes)
     data['age'] = data['age'].astype(int)
     data['platelets'] = data['platelets'].astype(int)
     data['serum_creatinine'] = data['serum_creatinine'].astype(int)
     data['time'] = data['time'].astype(int)
     data['creatinine_phosphokinase'] = data['creatinine_phosphokinase'].astype(int)
-    print('\n')
-    print('Tipos de datos luego de conversión: ')
-    print(data.dtypes)
-    print('\n')
+    # print('\n')
+    print('Tipos de datos corregidos!')
+    print('')
 
     # atipicos según IQR
     def EliminarAtipicos(nombre_columna, df: pd.DataFrame):
@@ -40,8 +42,8 @@ def LimpiarData(data: pd.DataFrame):
         iqr = q3 - q1
         lower_bound = q1 - 1.5 * iqr
         upper_bound = q3 + 1.5 * iqr
-        print(nombre_columna, ': ', str(lower_bound), ', ', str(upper_bound))
         df_cleaned = df[(df[nombre_columna] >= lower_bound) & (df[nombre_columna] <= upper_bound)]
+        print(nombre_columna, ': ', str(lower_bound), ', ', str(upper_bound))
         return df_cleaned.reset_index(drop=True)
 
     print('Rangos típicos según IQR: ')
@@ -51,26 +53,22 @@ def LimpiarData(data: pd.DataFrame):
     data = EliminarAtipicos('platelets', data)
     data = EliminarAtipicos('serum_sodium', data)
     data = EliminarAtipicos('time', data)
-    print('\n')
+    print('')
     print('Datos atípicos eliminados!')
-    print('\n')
+    print('')
 
     # verificando columnas de (1 y 0)
     def VerificarBinarios(columna, df: pd.DataFrame):
         conteo_valores = df[columna].value_counts()
-        if len(conteo_valores) == 2 and conteo_valores.sum() == len(df):
+        if len(conteo_valores) != 2:
             print(f'La columna {columna} no tiene problemas')
-        else:
-            print(f'La columna {columna} tiene valores atípicos')
 
-    print('Verificación de columnas binarias:')
     VerificarBinarios('anaemia', data)
     VerificarBinarios('smoking', data)
     VerificarBinarios('diabetes', data)
     VerificarBinarios('high_blood_pressure', data)
     VerificarBinarios('sex', data)
     VerificarBinarios('DEATH_EVENT', data)
-    print('\n')
 
     def CategorizarEdad(edad):
         if edad <= 12:
@@ -86,9 +84,10 @@ def LimpiarData(data: pd.DataFrame):
 
     data['Grupo de Edad'] = data['age'].apply(CategorizarEdad)
     print('Edades categorizadas!')
-    print('\n')
+    print('')
 
     data.to_csv('./resultado_limpieza.csv', index=False)
     print('Limpieza realizada!')
+    print('\n')
 
 LimpiarData(data_original)
